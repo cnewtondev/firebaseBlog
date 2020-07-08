ccd02e75da8833baf6f2444fc89cb20a001b827c
coltonnewton89 <52718393+coltonnewton89@users.noreply.github.com>
2020-07-07 22:27:19 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -79,3 +79,93 @@ class App extends Component {
 
  
 export default App;
+
+// import React, { Component } from 'react'
+// import firebase from './FireConfig'
+// import SunEditor from 'suneditor-react'
+// import 'suneditor/dist/css/suneditor.min.css';
+
+// class Create extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       title: '',
+//       body: '',
+//       posts: {}
+//     }
+//     this.handleTitleChange = this.handleTitleChange.bind(this)
+//     this.onInputChange = this.onInputChange.bind(this)
+//     this.onHandleSubmit = this.onHandleSubmit.bind(this)
+//   }
+
+//   handleTitleChange(e){
+//     this.setState({title: e})
+//     console.log(this.state.title)
+//   }
+
+//   onInputChange(e) {
+//     this.setState({
+//       body: e
+//     })
+//     console.log(this.state.body)
+//   }
+
+//   logUser = () => {
+//     const user = firebase.auth().currentUser.uid.toString()
+//     console.log(user)
+//   }
+
+//   onHandleSubmit(e) {
+//     e.preventDefault()
+//     const user = firebase.auth().currentUser.uid.toString()
+//     const post = {
+//       title: this.state.title,
+//       body: this.state.body
+//     }
+//     firebase.firebase()
+//       .ref(`/workshopPosts/${user}`).push(post)
+//     this.setState({
+//       title: '',
+//       body: ''
+//     })
+//     console.log(user)
+//   }
+
+//   render() {
+//     return (
+//     <div>
+//       <form onSubmit={this.onHandleSubmit}>
+//       <input
+//               type="text"
+//               name="title"
+//               placeholder="Title"
+//               onChange={this.onInputChange}
+//               ref="title"
+//               className="form-control"
+//             />
+//       <SunEditor setDefaultStyle="background-color: rgba(0, 0, 0,0)"
+//       setOptions={{
+//           buttonList: [
+//             ['undo', 'redo'],
+//             ['font', 'fontSize'],
+//             ['fontColor', 'bold', 'underline', 'italic'],
+//             ['image', 'video', 'link'],
+//             ['align', 'list'],
+//             ['table', 'template']
+//           ]
+//         }}
+//       placeholder='Body'
+//       value={
+//         this.state.body
+//       }
+//       onChange={
+//         this.onInputChange
+//       }/>
+//       <button>Post</button>
+//       </form> <br/>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Create;
